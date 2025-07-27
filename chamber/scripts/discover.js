bdef6c446f1cc87e67f956318130fe0ba86d80d8
Aurora Norlund <auroranorlund@gmail.com>
2025-07-27 00:24:40 -0700

    w5 maybe final

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,35 +1,72 @@
+// Landmark Cards
+
 const url = 'https://auroranorlund.github.io/wdd231/chamber/data/discover.json';
-const cards = document.querySelector('#areas');
+const areas = document.querySelector('#areas');
 async function getLandmarkData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.landmarks);
-    //displayProphets(data.landmarks)
+    displayLandmarks(data.landmarks);
 };
 
 const displayLandmarks = (landmarks) => {
   landmarks.forEach((landmark) => {
     let area = document.createElement("section");
     let name = document.createElement("h2");
-    let photoFigure = document.createElement("figure")
+    let photoFigure = document.createElement("figure");
     let photo = document.createElement("img");
     let address = document.createElement("address");
     let description = document.createElement("p");
+    let button = document.createElement("button");
 
     name.innerHTML = `${landmark.name}`;
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
-    portrait.setAttribute("loading", "lazy");
-    portrait.setAttribute("width", "340");
-    portrait.setAttribute("height", "440");
-    birthdate.innerHTML = `Date of Birth: ${prophet.birthdate}`;
-    birthplace.innerHTML = `Place of Birth: ${prophet.birthplace}`
 
-    card.appendChild(fullName);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
-    card.appendChild(portrait);
-    cards.appendChild(card);
+    photo.setAttribute("src", landmark.imageurl);
+    photo.setAttribute("alt", `Picture of ${landmark.name}`);
+    photo.setAttribute("loading", "lazy");
+    photo.setAttribute("width", "300");
+    photo.setAttribute("height", "200");
+    photoFigure.appendChild(photo);
+
+    address.innerHTML = `${landmark.address}`;
+    description.innerHTML = `${landmark.description}`;
+    button.innerHTML = `Learn More`;
+
+    area.appendChild(name);
+    area.appendChild(photoFigure);
+    area.appendChild(address);
+    area.appendChild(description);
+    area.appendChild(button);
+
+    areas.appendChild(area);
   });
 }
-getProphetData();
+
+getLandmarkData();
+
+// Display visits
+
+const visitsP = document.querySelector("#visits");
+let currentVisit = new Date();
+
+if (localStorage.getItem("lastVisit") === null) {
+  visitsP.innerHTML = `Welcome! Let us know if you have any questions.`;
+  localStorage.setItem("lastVisit", currentVisit);
+}
+else {
+  let lastVisit = window.localStorage.getItem("lastVisit");
+  let diff = currentVisit.getTime() - Date.parse(lastVisit);
+  console.log(diff);
+  const milPerDay = 1000 * 60 * 60 * 24;
+  diff = diff / milPerDay;
+  console.log(diff);
+  if (diff < 1) {
+    visitsP.innerHTML = `Back so soon! Awesome!`
+  }
+  else if (Math.round(diff) == 1) {
+    visitsP.innerHTML = `You last visited 1 day ago.`
+  }
+  else {
+    visitsP.innerHTML = `You last visited ${Math.round(diff)} days ago.`
+  }
+  localStorage.setItem("lastVisit", currentVisit);
+}
